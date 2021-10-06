7715d3221755cccb3d1ff92e93ae2acd4afb25fe
Nichita Railean <nichita.railean@aol.com>
2021-10-06 15:26:52 +0300

    Initial commit

---
+++ Assignment-1/Data/ITUserInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment_1.Data
+{
+    public interface ITUserInterface
+    {
+        User ValidateUser(string userName, string Password);
+        void AddUser(User user);
+    }
+}
---
+++ Assignment-1/Data/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_1.Data
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string pass { get; set; }
+        public int BirthYear { get; set; }
+        public int SecurityLevel { get; set; }
+    }
+}
